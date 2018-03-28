aa1828ab53f9afd98f49e2c5d30653f53cda1452
jameslin <kinokojiang@hotmail.com>
2018-03-28 11:36:43 +0800

    third

--- test/e2e/class/testStageProductPage.js
+++ test/e2e/class/testStageProductPage.js
@@ -4,11 +4,12 @@ module.exports = {
         browser //載入畫面
             .url('http://play.stage.niceday.tw:8200/product/3221')
             .waitForElementVisible('body',1000)
+            .windowMaximize()
         browser //展開簡介
             .useXpath()
             .click('//*[@id="introductionCkEditor"]/div[2]')
             .expect.element('//*[@id="introductionCkEditor"]').to.present
-        browser //檢查是否有顯示折扣內容
+        browser //檢查PromotionCard是否有顯示折扣內容
             .useCss()
             .getText('div.kCJRRA',function(res){
                 this.expect.element('div.jjGRGn').text.to.contain(res.value)
@@ -23,17 +24,31 @@ module.exports = {
             .useXpath()
             .click('//*[@id="__next"]/div/div/div/div[3]/div/div[2]/button')
             .expect.element('//*[@id="__next"]/div/div/div/section[3]/div/aside/div').to.present
-        browser //點擊timeRange顯示itemList
+        browser 
             .useXpath()
+                //  等5秒，不然流程無法進行 => 可能跟網站架構有關，正式環境不需要這個步驟
+            .pause('5000')
+                //點擊timeRange顯示itemList
             .click('//*[@id="__next"]/div/div/div/section[3]/div/aside/div/div[3]/div/div')
-            .saveScreenshot('./screenshots/index2.png')
-
-            // .expect.element('//*[@id="__next"]/div/div/div/section[3]/div/aside/div/div[3]/div/div').to.be.present
-            // .click('//*[@id="__next"]/div/div/div/section[3]/div/aside/div/div[4]/div[2]/div[2]/div/button[2]')
-            // .getText('div.bBNViN//*[@id="__next"]/div/div/div/section[3]/div/aside/div/div[5]/div[3]',function(res){
-            //     this.expect.element('//*[@id="__next"]/div/div/div/section[3]/div/article/div[3]/div[1]').text.be.equal(res.value)
-            // })
+                //點選一次產品項目
+            .click('//*[@id="__next"]/div/div/div/section[3]/div/aside/div/div[4]/div[2]/div[2]/div/button[2]')
+                //確認明細容與項目內容相同
+            .getText('//*[@id="__next"]/div/div/div/section[3]/div/article/div[3]/div[1]',function(result){
+                this.expect.element('//*[@id="__next"]/div/div/div/section[3]/div/aside/div/div[4]/div[2]').text.be.contain(result.value)
+            })
+                //確認優惠活動內容跟PromotionCard上顯示的優惠內容一致
+            .getText('//*[@id="__next"]/div/div/div/section[3]/div/aside/div/div[6]/div[1]/div[2]', function(result){
+                this.expect.element('//*[@id="__next"]/div/div/div/section[2]/div/aside/div/div[1]/div[4]').text.be.equal(result.value)
+            })
+                //確認交易金額與折扣後金額相同
+            .getText('//*[@id="__next"]/div/div/div/section[3]/div/aside/div/div[7]/div/div/div[2]/div[2]', function(result){
+                this.expect.element('//*[@id="__next"]/div/div/div/section[3]/div/aside/div/div[6]/div[1]/div[2]').text.to.match(/[^\W_]+/,result.value)
+            })
+                //確認送出訂單後到達結帳頁
+            .click('//*[@id="submitBtn"]')
+            .assert.urlEquals('http://play.stage.niceday.tw:8200/purchase/contacter')
         browser
+            .saveScreenshot('./screenshots/index2.png')
             .end()
     }
 }
